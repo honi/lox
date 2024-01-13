ca13c42a5cb020cb56154959d4f4efc1e6bc9590
Joni Bekenstein <joni@bek.io>
2024-01-13 11:49:24 -0300

    Organize imports

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -1,11 +1,11 @@
 package lox;
 
+import static lox.TokenType.*;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static lox.TokenType.*;
 
 class Scanner {
     private final String source;
